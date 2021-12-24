c53116d71bc92412e883f0cc9cb6af8d80e3f82a
fughesi <89326636+fughesi@users.noreply.github.com>
2021-12-24 14:47:09 -0500

    built out navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from "../images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <>
+        <div id="navContainer">
+            <img src={logo} alt="AirBnB company logo" />
+        </div>
+        </>
+    )
+}
