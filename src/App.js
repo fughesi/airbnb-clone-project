9750cf2bdae96066aa32bba0928a65a6bc3f058d
Steven <steven.camillieri@protonmail.com>
2021-12-26 03:54:14 -0500

    finished the challenge

--- src/App.js
+++ src/App.js
@@ -2,43 +2,28 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
 import data from "./data"
-// import Hero from "./components/Hero"
-// <Hero />
+import Hero from "./components/Hero"
 
 export default function App() {
 
-  console.log(data)
+
   const CardEl = data.map(info => 
     <Card 
-      img={info.coverImg}
-      rating={info.stats.rating}
-      reviewCount={`(${info.stats.reviewCount})`}
-      location={info.location}
-      title={info.title}
-      price={info.price}
-      description={info.description}
-    />
-    )
-
-  return (
-    <>
+    key={info.id}
+    info={info}
+    />)
+    
+    return (
+      <>
     <div id="container">
       <Navbar />
-      {CardEl}
+      <Hero />
+      <section id="carousel">
+        {CardEl}
+      </section>
     </div>
     </>
   );
 }
 
  
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,27 @@ import Star from "../images/star.png"
 
 
 export default function Card(props) {
+    console.log(props.info)
+    
+    let badgeText
+    if (props.info.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.info.location === "Online") {
+        badgeText = "Online"
+    }
+
     return (
         <>
         <div id="cardContainer">
-            {/* <h6>SOLD OUT</h6> */}
-            <img src={props.img} alt="." />
-            <div id="cardText">
-                <p><img src={Star} alt="star" /> {props.rating}<span>{props.reviewCount} • {props.location}</span></p>
-                <p>{props.title}</p>
-                <p><span>From ${props.price}</span> / person</p>
+            <div id="cardFlex">
+                {badgeText && <h6>{badgeText}</h6>}
+                <img src={props.info.coverImg} alt="." />
+                <div id="cardText">
+                    <p><img src={Star} alt="star" /> {props.info.stats.rating}<span> ({props.info.stats.reviewCount}) • {props.info.location}</span></p>
+                    <p>{props.info.title}</p>
+                    <p><span>From ${props.info.price}</span> / person</p>
+                </div>
             </div>
-            
         </div>
         </>
     )
